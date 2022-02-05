a5972706b2ad8e4f3f882223a8227d955c00a1a9
Polyscone <polyscone@gmail.com>
2022-02-05 10:59:38 +0900

    Initial commit

---
+++ cmd/adventure/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Adventure", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{X: 0, Y: 0, W: 200, H: 200}
+	surface.FillRect(&rect, 0xFFFFFFFF)
+
+	window.UpdateSurface()
+
+	var quit bool
+	for !quit {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				quit = true
+			default:
+				fmt.Printf("Event %T\n", event)
+			}
+		}
+	}
+}
