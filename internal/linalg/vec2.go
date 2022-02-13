6fe9c716310425f72dead49f6fafa764f1f6580f
Polyscone <polyscone@gmail.com>
2022-02-14 00:40:59 +0900

    Add a basic `Vec2` type

---
+++ internal/linalg/vec2.go
@@ -0,0 +1,76 @@
+package linalg
+
+import "math"
+
+type Vec2 struct {
+	X float64
+	Y float64
+}
+
+func New(x, y float64) Vec2 {
+	return Vec2{X: x, Y: y}
+}
+
+func (v Vec2) Add(rhs Vec2) Vec2 {
+	return Vec2{
+		X: v.X + rhs.X,
+		Y: v.Y + rhs.Y,
+	}
+}
+
+func (v Vec2) Sub(rhs Vec2) Vec2 {
+	return Vec2{
+		X: v.X - rhs.X,
+		Y: v.Y - rhs.Y,
+	}
+}
+
+func (v Vec2) Mul(rhs float64) Vec2 {
+	return Vec2{
+		X: v.X * rhs,
+		Y: v.Y * rhs,
+	}
+}
+
+func (v Vec2) Negate() Vec2 {
+	return Vec2{
+		X: -v.X,
+		Y: -v.Y,
+	}
+}
+
+func (v Vec2) Dot(rhs Vec2) float64 {
+	return v.X*rhs.X + v.Y*rhs.Y
+}
+
+func (v Vec2) MagnitudeSq() float64 {
+	// The result of the dot product of a vector with itself is the magnitude
+	// squared because the dot product is the scalar projection of the lhs
+	// vector onto the rhs vector which is then scaled by rhs vector's magnitude
+	return v.Dot(v)
+}
+
+func (v Vec2) Magnitude() float64 {
+	return math.Sqrt(v.MagnitudeSq())
+}
+
+func (v Vec2) Normalise() Vec2 {
+	// A zero vector cannot be normalised
+	if v.X == 0 && v.Y == 0 {
+		return v
+	}
+
+	reciprocal := 1.0 / v.Magnitude()
+
+	return v.Mul(reciprocal)
+}
+
+func (v Vec2) Reflect(normal Vec2) Vec2 {
+	// We assume the normal is already a unit vector, hence the name "normal"
+	// The reason for this is that if the user has already normalised the vector
+	// then we would be wasting cycles re-normalising it again
+	scalarProjectionDoubled := v.Dot(normal) * 2.0
+	normalScaled := normal.Mul(scalarProjectionDoubled)
+
+	return normalScaled.Sub(v)
+}
